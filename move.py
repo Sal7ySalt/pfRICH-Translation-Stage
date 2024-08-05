bbc197a32535b9095f3b19adda649cfffc74e47f
Sal7ySalt <danielzhang309@yahoo.com>
2024-08-05 17:05:03 -0400

    Update move.py

--- move.py
+++ move.py
@@ -1,97 +1,56 @@
 import serial
 import time
 from binascii import unhexlify
+from setup.py import setup
 
-def move():
+setup()
 
-    #open serial port
-    #For Linux
-    # use 'dmesg | grep tty' to find serial port
-    # eventually need to change permissions for /dev/tty*
-    # uncomment the following lines
-    ser=serial.Serial('/dev/ttyUSB0', baudrate=115200,bytesize=8,
-                      parity=serial.PARITY_NONE,
-                      stopbits=1,xonxoff=0,
-                      rtscts=0,
-                      timeout=1)  
+#Set Velocity; MGMSG_MOT_SET_VELPARAMS
+Velocity = "1304"
+MinVelocity = 0 #mm/s
+Acceleration = 1 #mm/s²
+MaxVelocity = 1.5 #mm/s
 
-    Channel = "01" #Channel is always 1 for a K Cube/T Cube
-    ChanIdent = "0100" #Channel Ident if not in Header
-    Destination = "50" #Destination; 50 for T Cube/K Cube, USB controllers
-    DestinationI = "D0" #Logic Or Destination; D0 for T Cube/K Cube, USB controllers
-    Source = "01" #Source
-    Device_Unit_SF = 34304.96 #pg 34 of protocal PDF (as of Issue 23)
-    Device_Unit_Vel = 772981.3692
-    Device_Unit_Acc = 263.8443072
+MinVelocityDU = round(Device_Unit_Vel * MinVelocity)
+AccelerationDU = round(Device_Unit_Acc * Acceleration)
+MaxVelocityDU = round(Device_Unit_Vel * MaxVelocity)
 
+ser.write(unhexlify(Velocity + "0E00" + DestinationI + Source + ChanIdent) + MinVelocityDU.to_bytes(4, byteorder="little") + AccelerationDU.to_bytes(4, byteorder="little") + MaxVelocityDU.to_bytes(4, byteorder="little"))
 
-    print(ser.is_open)
-    ser.flushInput()
-    ser.flushOutput()
+AbsDistance = 20 #mm
+AbsDistanceDU = round(Device_Unit_SF * AbsDistance)
 
-    command = bytearray([0x23, 0x02, 0x00, 0x00, 0x50, 0x01])  # MGMSG_MOD_IDENTIFY
-    ser.write(command)
-    ser.flushInput()
-    ser.flushOutput()
-    time.sleep(1)
-    print("test1")
+#Move Stage; MGMSG_MOT_MOVE_ABSOLUTE
+Move = "5304"
+print("Moving Stage")
+ser.write(unhexlify(Move + "0600" + DestinationI + Source + ChanIdent) + AbsDistanceDU.to_bytes(4, byteorder="little"))
 
-    ser.write(bytearray([0x10, 0x02, 0x01, 0x01, 0x50, 0x01]))  # MGMSG_MOD_SET_CHANENABLESTATE
-    ser.flushInput()
-    ser.flushOutput()
-    time.sleep(1)
-    print("test2")
+#Confirm stage moved before advancing; MGMSG_MOT_MOVE_COMPLETED
+Rx = ''
+Completed = unhexlify("6404")
+while Rx != Completed:
+    Rx = ser.read(2)
+print('Finished Moving')
+ser.flushInput()
+ser.flushOutput()
 
-    #Set Velocity; MGMSG_MOT_SET_VELPARAMS
-    Velocity = "1304"
-    MinVelocity = 0 #mm/s
-    Acceleration = 1 #mm/s²
-    MaxVelocity = 1.5 #mm/s
+#Get Position; MGMSG_MOT_REQ_USTATUSUPDATE
+Status = "9004"
+ser.write(unhexlify(Status + Channel + "00" + Destination + Source))
 
-    MinVelocityDU = round(Device_Unit_Vel * MinVelocity)
-    AccelerationDU = round(Device_Unit_Acc * Acceleration)
-    MaxVelocityDU = round(Device_Unit_Vel * MaxVelocity)
+Rx = ser.read(20)
 
-    ser.write(unhexlify(Velocity + "0E00" + DestinationI + Source + ChanIdent) + MinVelocityDU.to_bytes(4, byteorder="little") + AccelerationDU.to_bytes(4, byteorder="little") + MaxVelocityDU.to_bytes(4, byteorder="little"))
+Position = Rx[8:12]
 
-    AbsDistance = 20 #mm
-    AbsDistanceDU = round(Device_Unit_SF * AbsDistance)
+PositionDU = int.from_bytes(Position, "little")
+print("Position:", PositionDU, "Device Units" )
+PositionRU = PositionDU / 34304
+print("Position:", PositionRU, "mm")
 
-    #Move Stage; MGMSG_MOT_MOVE_ABSOLUTE
-    Move = "5304"
-    print("Moving Stage")
-    ser.write(unhexlify(Move + "0600" + DestinationI + Source + ChanIdent) + AbsDistanceDU.to_bytes(4, byteorder="little"))
-
-    #Confirm stage moved before advancing; MGMSG_MOT_MOVE_COMPLETED
-    Rx = ''
-    Completed = unhexlify("6404")
-    while Rx != Completed:
-        Rx = ser.read(2)
-    print('Finished Moving')
-    ser.flushInput()
-    ser.flushOutput()
-
-    #Get Position; MGMSG_MOT_REQ_USTATUSUPDATE
-    Status = "9004"
-    ser.write(unhexlify(Status + Channel + "00" + Destination + Source))
-
-    Rx = ser.read(20)
-
-    Position = Rx[8:12]
-
-    PositionDU = int.from_bytes(Position, "little")
-    print("Position:", PositionDU, "Device Units" )
-    PositionRU = PositionDU / 34304
-    print("Position:", PositionRU, "mm")
-
-    #Disable Stage; MGMSG_MOD_SET_CHANENABLESTATE
-    Enable = "1002"
-    State = "02"
-    ser.write(unhexlify(Enable + Channel + State + Destination + Source))
+#Disable Stage; MGMSG_MOD_SET_CHANENABLESTATE
+Enable = "1002"
+State = "02"
+ser.write(unhexlify(Enable + Channel + State + Destination + Source))
 
 
-    ser.close()
-
-if __name__ == "__main__":
-    move()
-
+ser.close()
