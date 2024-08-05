87c676e2844465633ac0f0f55379669cba311f27
Sal7ySalt <danielzhang309@yahoo.com>
2024-08-05 17:04:12 -0400

    Update home.py

--- home.py
+++ home.py
@@ -1,82 +1,42 @@
 import serial
 import time
 from binascii import unhexlify
+from setup.py import setup
 
-def home():
+setup()
 
-    #open serial port
-    #For Linux
-    # use 'dmesg | grep tty' to find serial port
-    # eventually need to change permissions for /dev/tty*
-    # uncomment the following lines
-    ser=serial.Serial('/dev/ttyUSB0', baudrate=115200,bytesize=8,
-                      parity=serial.PARITY_NONE,
-                      stopbits=1,xonxoff=0,
-                      rtscts=0,
-                      timeout=1)  
+#Home Stage; MGMSG_MOT_MOVE_HOME
+Home = "4304"
+print("Homing")
+ser.write(unhexlify(Home + Channel + "00" + Destination + Source))
 
-    Channel = "01" #Channel is always 1 for a K Cube/T Cube
-    ChanIdent = "0100" #Channel Ident if not in Header
-    Destination = "50" #Destination; 50 for T Cube/K Cube, USB controllers
-    DestinationI = "D0" #Logic Or Destination; D0 for T Cube/K Cube, USB controllers
-    Source = "01" #Source
-    Device_Unit_SF = 34304.96 #pg 34 of protocal PDF (as of Issue 23)
-    Device_Unit_Vel = 772981.3692
-    Device_Unit_Acc = 263.8443072
+#Confirm stage homed before advancing; MGMSG_MOT_MOVE_HOMED
+Rx = ''
+Homed = unhexlify("4404")
+while Rx != Homed:
+    Rx = ser.read(2)
+print('Stage Homed')
+ser.flushInput()
+ser.flushOutput()
 
+#Get Position; MGMSG_MOT_REQ_USTATUSUPDATE
+Status = "9004"
+ser.write(unhexlify(Status + Channel + "00" + Destination + Source))
 
-    print(ser.is_open)
-    ser.flushInput()
-    ser.flushOutput()
+Rx = ser.read(20)
 
-    command = bytearray([0x23, 0x02, 0x00, 0x00, 0x50, 0x01])  # MGMSG_MOD_IDENTIFY
-    ser.write(command)
-    ser.flushInput()
-    ser.flushOutput()
-    time.sleep(1)
-    print("test1")
+Position = Rx[8:12]
 
-    ser.write(bytearray([0x10, 0x02, 0x01, 0x01, 0x50, 0x01]))  # MGMSG_MOD_SET_CHANENABLESTATE
-    ser.flushInput()
-    ser.flushOutput()
-    time.sleep(1)
-    print("test2")
+PositionDU = int.from_bytes(Position, "little")
+print("Position:", PositionDU, "Device Units" )
+PositionRU = PositionDU / 34304
+print("Position:", PositionRU, "mm")
 
-    #Home Stage; MGMSG_MOT_MOVE_HOME
-    Home = "4304"
-    print("Homing")
-    ser.write(unhexlify(Home + Channel + "00" + Destination + Source))
-
-    #Confirm stage homed before advancing; MGMSG_MOT_MOVE_HOMED
-    Rx = ''
-    Homed = unhexlify("4404")
-    while Rx != Homed:
-        Rx = ser.read(2)
-    print('Stage Homed')
-    ser.flushInput()
-    ser.flushOutput()
-
-    #Get Position; MGMSG_MOT_REQ_USTATUSUPDATE
-    Status = "9004"
-    ser.write(unhexlify(Status + Channel + "00" + Destination + Source))
-
-    Rx = ser.read(20)
-
-    Position = Rx[8:12]
-
-    PositionDU = int.from_bytes(Position, "little")
-    print("Position:", PositionDU, "Device Units" )
-    PositionRU = PositionDU / 34304
-    print("Position:", PositionRU, "mm")
-
-    #Disable Stage; MGMSG_MOD_SET_CHANENABLESTATE
-    Enable = "1002"
-    State = "02"
-    ser.write(unhexlify(Enable + Channel + State + Destination + Source))
+#Disable Stage; MGMSG_MOD_SET_CHANENABLESTATE
+Enable = "1002"
+State = "02"
+ser.write(unhexlify(Enable + Channel + State + Destination + Source))
 
 
-    ser.close()
-
-if __name__ == "__main__":
-    home()
+ser.close()
 
